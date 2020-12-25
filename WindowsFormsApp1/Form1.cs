35d6d283d8e612036dff343487b420d9646e6120
georgianikita <nikitaherawati@gmail.com>
2020-12-25 23:28:32 +0700

    fin1

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -91,38 +91,28 @@ namespace WindowsFormsApp1
             Invalidate();
             
         }
-        
+
         private void triggerGerakAi()
         {
-                if (turn)
-                {
-                    if (mode == MODE_AWvP || mode == MODE_AvA)
-                    {
-                        papan.gerakAI(turn);
-                        turn = !turn;
-                        checkGameEnded();
-
-                    }
-
-                }
-                else
-                {
-                    if (mode == MODE_ABvP || mode == MODE_AvA)
-                    {
-                        papan.gerakAI(turn);
-                        turn = !turn;
-                        checkGameEnded();
-                    }
-                }
-            
+            bool isWhite = turn;
+            bool isAiGerak = (mode == MODE_AvA) || // Jika Mode AI vs AI
+                (isWhite && mode == MODE_AWvP) || // Jika Turn White & Mode AI WHite
+                (!isWhite && mode == MODE_ABvP); // Jika Turn Black & Mode AI Black
+            // Jika AI Gerak
+            if (isAiGerak)
+            {
+                CheckForIllegalCrossThreadCalls = false;
+                fin = true; // Supaya User tidak bisa click
+                new System.Threading.Thread(AiGerak).Start();
+            }
         }
         public void checkGameEnded()
         {
             int winner = papan.getWinner();
             if (winner != Catur.SPACE)
             {
-                MessageBox.Show("WINNER " + (winner == Catur.WHITE ? "WHITE !" : "BLACK !"));
                 fin = true;
+                MessageBox.Show("WINNER " + (winner == Catur.WHITE ? "WHITE !" : "BLACK !"));
             }
             else
             {
@@ -130,8 +120,21 @@ namespace WindowsFormsApp1
             }
         }
 
+        void AiGerak()
+        {
+            // Proses Makan Waktu
+            papan.gerakAI(turn);
+            // ...
+            // Jika sudah selesai maka...
+            fin = false;
+            turn = !turn;
+            Invalidate();
+            checkGameEnded();
+        }
+
         private void timer1_Tick(object sender, EventArgs e)
         {
         }
+        
     }
 }
